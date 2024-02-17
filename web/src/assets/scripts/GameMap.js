9786c675b01a5210ee6e1882f5bdff646bf19945
zbw <1065346495@qq.com>
2024-02-18 02:01:33 +0800

    非法判定 + 画蛇眼

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -127,10 +127,29 @@ export class GameMap extends GameObject {
         return true;
     }
 
+    check_valid(cell){ // 检测目标位置是否合法
+        for(const wall of this.walls){
+            if(wall.r === cell.r && wall.c === cell.c)
+                return false;
+        }
+
+        for(const snake of this.snakes){
+            let k = snake.cells.length;
+            if(!snake.check_tail_increasing()){
+                k--;
+            }
+            for(let i = 0; i < k; i++){
+                if(snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
     next_step(){//进入下一回合
         for(const snake of this.snakes){
             snake.next_step();
-            console.log("next_step");
         }
     }
 
--- web/src/assets/scripts/Snake.js
+++ web/src/assets/scripts/Snake.js
@@ -21,6 +21,23 @@ export class Snake extends GameObject{
 
         this.step = 0; //回合数
         this.eps = 1e-2;// 允许的误差
+
+        //蛇眼朝向，ID：0初始朝上 ID：1初始朝下
+        this.eye_direction = 0;
+        if(this.id === 1) this.eye_direction = 2;
+
+        this.eye_dx = [
+            [-1, 1],
+            [1, 1],
+            [-1, 1],
+            [-1, -1],
+        ];
+        this.eye_dy = [
+            [-1, -1],
+            [-1, 1],
+            [1, 1],
+            [-1, 1],
+        ];
     }
 
     start(){
@@ -48,6 +65,12 @@ export class Snake extends GameObject{
         for(let i = k; i > 0; i--){
             this.cells[i] = JSON.parse(JSON.stringify(this.cells[i - 1]));
         }
+
+        if(!this.gamemap.check_valid(this.next_cell)){
+            this.status = "die";
+        }
+
+        this.eye_direction = d;
     }
 
     update_move(){
@@ -98,6 +121,10 @@ export class Snake extends GameObject{
 
         //画蛇（圆）
         ctx.fillStyle = this.color;
+        if(this.status === "die"){
+            ctx.fillStyle = "white";
+        }
+
         for(const cell of this.cells){
             ctx.beginPath();
             ctx.arc(cell.x * L, cell.y * L, L/2, 0, Math.PI * 2);
@@ -113,6 +140,16 @@ export class Snake extends GameObject{
             else{
                 ctx.fillRect(Math.min(a.x, b.x) * L, (a.y - 0.5) * L, Math.abs(a.x - b.x) * L, L)
             }
+        }
+
+        //点睛
+        ctx.fillStyle = "black";
+        for(let i = 0; i < 2; i++){
+            const eye_x = (this.cells[0].x + this.eye_dx[this.eye_direction][i] * 0.25) * L;
+            const eye_y = (this.cells[0].y + this.eye_dy[this.eye_direction][i] * 0.25) * L;
+            ctx.beginPath();
+            ctx.arc(eye_x, eye_y, L * 0.1, 0, Math.PI * 2);
+            ctx.fill();
         }
     }
 }
