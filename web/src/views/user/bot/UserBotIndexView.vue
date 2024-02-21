3cbf88824a827dd92373ea89674f47f2a14adc64
zbw <1065346495@qq.com>
2024-02-22 00:19:15 +0800

    实现了bot管理的后端接口

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -4,10 +4,81 @@
 
 <script>
 import ContentField from "../../../components/ContentField"
+import $ from 'jquery'
+import { useStore } from 'vuex'
 
 export default{
   components: {
     ContentField
+  },
+  setup() {
+    const store = useStore();
+    // $.ajax({
+    //     url: "http://localhost:3000/user/bot/add/",
+    //     type: "post",
+    //     data: {
+    //       title: "我的bot",
+    //       description: "bot描述",
+    //       content:  "bot代码",
+    //     },
+    //     headers: {
+    //       Authorization: "Bearer " + store.state.user.token,
+    //     },
+    //     success(resp) {
+    //       console.log(resp);
+    //     },
+    //     error(resp){
+    //       console.log(resp);
+    //     }
+    //   })
+      // $.ajax({
+      //   url: "http://localhost:3000/user/bot/remove/",
+      //   type: "post",
+      //   data: {
+      //     bot_id: 5,
+      //   },
+      //   headers: {
+      //     Authorization: "Bearer " + store.state.user.token,
+      //   },
+      //   success(resp) {
+      //     console.log(resp);
+      //   },
+      //   error(resp){
+      //     console.log(resp);
+      //   }
+      // })
+      // $.ajax({
+      //   url: "http://localhost:3000/user/bot/update/",
+      //   type: "post",
+      //   data: {
+      //     bot_id: 6,
+      //     title: "我的新bot",
+      //     description: "bot新描述",
+      //     content:  "bot新代码",
+      //   },
+      //   headers: {
+      //     Authorization: "Bearer " + store.state.user.token,
+      //   },
+      //   success(resp) {
+      //     console.log(resp);
+      //   },
+      //   error(resp){
+      //     console.log(resp);
+      //   }
+      // })
+      $.ajax({
+        url: "http://localhost:3000/user/bot/getlist/",
+        type: "get",
+        headers: {
+          Authorization: "Bearer " + store.state.user.token,
+        },
+        success(resp) {
+          console.log(resp);
+        },
+        error(resp){
+          console.log(resp);
+        }
+      })
   }
 }
 
